c5833056e139e888a137ec1b2b5153b3d6ee8dc0
aux-safran <safran.faiz@1billiontech.com>
2020-11-09 07:40:32 +0530

    registering as an eureka client

--- src/main/java/com/example/demoUser/DemoUserApplication.java
+++ src/main/java/com/example/demoUser/DemoUserApplication.java
@@ -2,8 +2,11 @@ package com.example.demoUser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoUserApplication {
 
 	public static void main(String[] args) {
